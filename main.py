d204398c4d5bd887d17ce52367810312798de4f8
Gonzalo Jimenez <gonzalojh596@gmail.com>
2019-09-30 19:55:56 +0200

    find_squares

--- main.py
+++ main.py
@@ -74,8 +74,21 @@ class Webcam:
 
         #Adquirimos el vector con los puntos de los contornos
         contours, _hierarchy = cv2.findContours(self.cv_video[1], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        squares = []
+        for cnt in contours:
+            cnt_len = cv2.arcLength(cnt, True)
+            cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+            if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                cnt = cnt.reshape(-1, 2)
+                max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
+                if max_cos < 0.1:
+                    squares.append(cnt)
+        cv2.drawContours( self.cv_video[1], squares, -1, (0, 255, 0), 3 )
+        cv2.imshow('squares', self.cv_video[1])
 
-
+    def angle_cos(p0, p1, p2):
+        d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+        return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
 
 
     def show_frames(self):

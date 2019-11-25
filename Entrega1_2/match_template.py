762a59f376c393d906073960578c0ec75917ee4a
Gonzalo Jimenez <gonzalojh596@gmail.com>
2019-11-25 21:21:52 +0100

    match_template arreglado

--- Entrega1_2/match_template.py
+++ Entrega1_2/match_template.py
@@ -1,37 +1,51 @@
 import cv2
 
-def Main():
-    template_img = cv2.imread("lena_eye.jpg", CV_LOAD_IMAGE_GRAYSCALE)
-    if (template_img.data == null):
-        return -1
-    cv2.imshow("Template Image", template_img)
+class Match_Template:
 
-    src_img = imread("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE)
-    if (src_img.data == null):
-		return -1
-    cv2.cvtColor(src_img, debug_img, CV_GRAY2BGR)
+    def __init__(self):
+        self.template_img = None
+        self.src_img = None
+        self.result_mat = None
+        self.minVal = None
+        self.maxVal = None
+        self.minLoc = None
+        self.maxLoc = None
 
-    match_method = CV_TM_CCORR_NORMED
-    cv2.matchTemplate(src_img, template_img, result_mat, match_method)
-    cv2.normalize(result_mat, result_mat, 0, 1, NORM_MINMAX, -1, cv2.Mat())
+    def readTemplateImage(self):
+        self.template_img = cv2.imread("lena_eye.jpg", cv2.IMREAD_GRAYSCALE)
+        if (self.template_img.data == None):
+            return -1
+        cv2.imshow("Template Image", self.template_img)
 
-    cv2.minMaxLoc(result_mat, minVal, maxVal, minLoc, maxLoc, Mat())
-    if(match_method == CV_TM_SQDIFF or match_method == CV_TM_SQDIFF_NORMED):
-        matchLoc = minLoc
-    else:
-        matchLoc = maxLoc
-    
-    cv2.rectangle(
-			debug_img,
-			matchLoc,
-			cv2.Point(matchLoc.x + template_img.cols , matchLoc.y + template_img.rows),
-			CV_RGB(255,0,0),
-			3)
-    cv2.imshow("Match Template", debug_img)
+    def readSrcImage(self):
+        self.src_img = cv2.imread("lena.jpg", cv2.IMREAD_GRAYSCALE)
+        if (self.src_img.data == None):
+            return -1
 
-    c = cv2.waitKey()
-    
-    return 0
+    def matchTemplate(self):
+        self.readTemplateImage()
+        self.readSrcImage()
+        match_method = cv2.TM_CCORR_NORMED
+        #match_method = cv2.TM_SQDIFF
+        self.result_mat = cv2.matchTemplate(self.src_img, self.template_img, match_method)
+        cv2.normalize(self.result_mat, self.result_mat, 0, 1, cv2.NORM_MINMAX, -1)
+
+        self.minVal, self.maxVal, self.minLoc, self.maxLoc = cv2.minMaxLoc(self.result_mat, None)
+        if(match_method == cv2.TM_SQDIFF or match_method == cv2.TM_SQDIFF_NORMED):
+            matchLoc = self.minLoc
+        else:
+            matchLoc = self.maxLoc
+        
+        cv2.rectangle(
+                self.src_img,
+                matchLoc,
+                (matchLoc[0] + self.template_img.shape[1] , matchLoc[1] + self.template_img.shape[0]),
+                (255,0,0),
+                3)
+        cv2.imshow("Match Template", self.src_img)
+
+        c = cv2.waitKey()
 
 if __name__ == "__main__":
-    Main()
+    m = Match_Template()
+    m.matchTemplate()

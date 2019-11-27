9c8a272ae47886428c3bd411b30edfc657b439cc
Gonzalo Jimenez <gonzalojh596@gmail.com>
2019-11-27 20:58:56 +0100

    blabla

--- Entrega1_2/main.py
+++ Entrega1_2/main.py
@@ -7,6 +7,12 @@ import math
 
 NUM_LADOS=4
 
+rectangleAreas=[                #x, y, ancho, alto
+        (1030,564,400,140),
+        (1030,850,400,140),
+        (1030,1144,400,140)
+    ]
+
 class Window:
 
     def __init__(self):
@@ -37,7 +43,8 @@ class Window:
         original_image = cv2.imread(str(fn[0]), cv2.IMREAD_COLOR)
         self.mat_original = cv2.resize(original_image, (720, 540), cv2.INTER_CUBIC)
 
-        self.make_filter()
+        w, h = rectangleAreas[0][2], rectangleAreas[0][3]
+        #self.make_filter()
         image = QtGui.QImage(self.mat_original, 720, 540, QtGui.QImage.Format_RGB888)
         pixmap = QtGui.QPixmap()
         pixmap.convertFromImage(image.rgbSwapped())
@@ -45,9 +52,12 @@ class Window:
         
 
     def make_filter(self):
+        self.image_gray = cv2.cvtColor(self.mat_original, cv2.COLOR_BGR2GRAY)
         #Asignamos al vídeo 1 la imagen con filtro gaussiano y canny
-        img = cv2.GaussianBlur(self.mat_original, (5,5),0)
-        tempV = cv2.Canny(img, 60, 60)
+        img = cv2.GaussianBlur(self.image_gray, (5,5),10)
+        tempV = cv2.Canny(img, 1, 1)
+
+        cv2.imshow("Imagen gris", self.image_gray)
 
         #Adquirimos el vector con los puntos de los contornos
         contours, _hierarchy = cv2.findContours(tempV, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -60,13 +70,13 @@ class Window:
             cnt = cv2.approxPolyDP(cnt, 0.05*cnt_len, True)
             #Primero comprueba que tiene 4 puntos, es decir 4 vértices, 4 lados. Despues establece el minimo de tamaño, a menor mas sensible.
             #Por ultimo comprueba si la silueta es convexa, es decir, no tiene angulos internos > 180 y no tiene diagonales interiores
-            if len(cnt) == NUM_LADOS and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+            """if len(cnt) == NUM_LADOS and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
                 cnt = cnt.reshape(-1, 2)
                 #Calcula el coseno máximo de entre los 4 puntos de cada silueta, el % es para iterar dando la vuelta a la lista de los 4 puntos
                 max_cos = np.max([self.angle_cos( cnt[i], cnt[(i+1) % NUM_LADOS], cnt[(i+2) % NUM_LADOS] ) for i in range(NUM_LADOS)])
                 #Comprueba que el angulo sea de 90 grados, con holgura para poder pillarlo en diagonal
-                if max_cos < 0.2:
-                    squares.append(cnt)
+                if max_cos < 0.2:"""
+            squares.append(cnt)
         self.isSquare(squares)
 
     def angle_cos(self, p0, p1, p2):
@@ -74,17 +84,10 @@ class Window:
         return abs( np.dot(d1, d2) / (np.sqrt(np.dot(d1,d1)) * np.sqrt(np.dot(d2,d2))))
 
     def isSquare(self, squares):
-        minVar = 0.90
-        maxVar = 1.1
-        dist = lambda p1, p2: math.sqrt( abs((p1[0]-p2[0])**2)+abs((p1[1]-p2[1])**2) )
-        cuadrados, rectangulos = [], []
+        rectangulos = []
         for cnt in squares:
-            if minVar < dist(cnt[0],cnt[1])/dist(cnt[1],cnt[2]) and dist(cnt[0],cnt[1])/dist(cnt[1],cnt[2]) < maxVar:
-                cuadrados.append(cnt)
-            else:
-                rectangulos.append(cnt)
-        cv2.drawContours( self.mat_original, cuadrados, -1, (0, 255, 0), 4 )
-        cv2.drawContours( self.mat_original, rectangulos, -1, (0, 0, 255), 4 )
+            rectangulos.append(cnt)
+        cv2.drawContours( self.mat_original, rectangulos, -1, (0, 0, 255), 1 )
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- Entrega1_2/matchTemplate.py
+++ Entrega1_2/matchTemplate.py
@@ -7,16 +7,17 @@ img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 template = cv2.imread('templates/1.png',0)
 w, h = template.shape[::-1]
 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED, N_object=float("inf"), score_threshold=0.5, maxOverlap=0.25, searchBox=None)
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 threshold = 0.8
 loc = np.where( res >= threshold)
 i = 0
 aux = 0
 for pt in zip(*loc[::-1]):
-    #if((pt[1]+w-aux) > 30):
-    #aux = pt[1]+w
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    print(pt) 
-    i+=1
+    print(pt[1]+w)
+    if((pt[1]+w-aux) > 30):
+        aux = pt[1]+w
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+        print(pt) 
+        i+=1
 print(i)
 cv2.imwrite('res.png',img_rgb)

356f506c1bc65626e8fa91689286ec3cbe331910
Gonzalo Jimenez <gonzalojh596@gmail.com>
2019-11-29 09:58:26 +0100

    pa entregá

--- Entrega1_2/main.py
+++ Entrega1_2/main.py
@@ -45,13 +45,9 @@ class Window:
             self.loadImage("capturas/capturas_{}.jpg".format(i+1))
             self.clipping()
             self.extractNumbers()
+        
         self.MainWindow.viewer_original.setText("GLOBAL PROCESS COMPLETE, CHECK out.txt")
-        self.MainWindow.viewer_counter1.clear()
-        self.MainWindow.viewer_counter2.clear()
-        self.MainWindow.viewer_counter3.clear()
-        self.res1.clear()
-        self.res2.clear()
-        self.res3.clear()
+        self.cleanLabels()
 
 
     def extractNumbers(self):
@@ -106,60 +102,22 @@ class Window:
         
         self.original_image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
         self.mat_original = cv2.resize(self.original_image, (720, 540), cv2.INTER_CUBIC)
-        self.make_filter()
 
+        self.cleanLabels()
+
+        image = QtGui.QImage(self.mat_original, 720, 540, QtGui.QImage.Format_RGB888)
+        pixmap = QtGui.QPixmap()
+        pixmap.convertFromImage(image.rgbSwapped())
+        self.MainWindow.viewer_original.setPixmap(pixmap)
+
+    def cleanLabels(self):
         self.MainWindow.viewer_counter1.clear()
         self.MainWindow.viewer_counter2.clear()
         self.MainWindow.viewer_counter3.clear()
         self.res1.clear()
         self.res2.clear()
         self.res3.clear()
-
-        image = QtGui.QImage(self.mat_original, 720, 540, QtGui.QImage.Format_RGB888)
-        pixmap = QtGui.QPixmap()
-        pixmap.convertFromImage(image.rgbSwapped())
-        self.MainWindow.viewer_original.setPixmap(pixmap)
         
-
-    def make_filter(self):
-        self.image_gray = cv2.cvtColor(self.mat_original, cv2.COLOR_BGR2GRAY)
-        #Asignamos al vídeo 1 la imagen con filtro gaussiano y canny
-       # img = cv2.GaussianBlur(self.image_gray, (5,5),0)
-       # tempV = cv2.threshold(img, 10, 250, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
-        self.image_gray = cv2.medianBlur(self.image_gray, 5)
-        tempV = cv2.adaptiveThreshold(self.image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-        #tempV = cv2.Canny(thres, 1, 1)
-
-        #Adquirimos el vector con los puntos de los contornos
-        contours, _hierarchy = cv2.findContours(tempV, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        squares = []
-        #Para cada contorno
-        for cnt in contours:
-            #Calcula la longitud total del contorno
-            cnt_len = cv2.arcLength(cnt, True)
-            #Aproxima a la silueta mas simple posible
-            cnt = cv2.approxPolyDP(cnt, 0.05*cnt_len, True)
-            #Primero comprueba que tiene 4 puntos, es decir 4 vértices, 4 lados. Despues establece el minimo de tamaño, a menor mas sensible.
-            #Por ultimo comprueba si la silueta es convexa, es decir, no tiene angulos internos > 180 y no tiene diagonales interiores
-            if len(cnt) == NUM_LADOS and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
-                cnt = cnt.reshape(-1, 2)
-                #Calcula el coseno máximo de entre los 4 puntos de cada silueta, el % es para iterar dando la vuelta a la lista de los 4 puntos
-                max_cos = np.max([self.angle_cos( cnt[i], cnt[(i+1) % NUM_LADOS], cnt[(i+2) % NUM_LADOS] ) for i in range(NUM_LADOS)])
-                #Comprueba que el angulo sea de 90 grados, con holgura para poder pillarlo en diagonal
-                if max_cos < 0.2:
-                    squares.append(cnt)
-        self.isSquare(squares)
-
-    def angle_cos(self, p0, p1, p2):
-        d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
-        return abs( np.dot(d1, d2) / (np.sqrt(np.dot(d1,d1)) * np.sqrt(np.dot(d2,d2))))
-
-    def isSquare(self, squares):
-        rectangulos = []
-        for cnt in squares:
-            rectangulos.append(cnt)
-        cv2.drawContours( self.mat_original, rectangulos, -1, (0, 0, 255), 1 )
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
--- Entrega1_2/matchTemplate.py
+++ Entrega1_2/matchTemplate.py
@@ -6,34 +6,19 @@ import MTM
 class MatchTemplate:
     def __init__(self):
         pass
+
     def doMatch(self, img_rgb):
-        #img_rgb = cv2.imread('capturas/capturas_5.jpg')
         img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
         templates = [['{}'.format(i), cv2.imread('./templates/{}.png'.format(i),0)]for i in range (10)]
         
-        #w, h = templates[i].shape[::-1]
-        #res = cv2.matchTemplate(img_gray,templates[i],cv2.TM_CCOEFF_NORMED)
         res = MTM.matchTemplates(templates, img_gray, method=cv2.TM_CCOEFF_NORMED, N_object=4, score_threshold=0.3, maxOverlap=0.4, searchBox=None)
         print(res)
         img_rgb = MTM.drawBoxesOnRGB(img_rgb, res, boxThickness=2, boxColor=(255, 255, 00), showLabel=True, labelColor=(255, 255, 0), labelScale=0.5 )
-        """
-        threshold = 0.8
-        loc = np.where( res >= threshold)
-        i = 0
-        aux = 0
-        for pt in zip(*loc[::-1]):
-            if((pt[1]+w-aux) > 30):
-                aux = pt[1]+w
-                cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-                i+=1
-                """
         num = []
         for i in range(len(res)):
             num.append([res['BBox'][i][0], res['TemplateName'][i]])
-        print(num)
         num.sort(key = self.sortFirst)
-        print(num)
-        cv2.imwrite('res.png',img_rgb)
         return img_rgb, num
+    
     def sortFirst(self,val):
         return val[0]
